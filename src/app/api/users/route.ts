56fcf487ffa2deec516e16261161981d03ee55fd
Jospin <jospinndagano1@gmail.com>
2025-06-30 11:23:03 +0200

    add user role

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -30,8 +30,6 @@ export async function GET(request: Request) {
           id: true,
           email: true,
           username: true,
-          firstName: true,
-          lastName: true,
           role: true,
           createdAt: true,
         }
